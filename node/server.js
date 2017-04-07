f9c6f4f5f3901e61af71c6e75612acc9e920aa6f
dtepdc <dawn714@gmail.com>
2017-04-07 17:42:32 -0400

    Movie Project

---
+++ node/server.js
@@ -0,0 +1,53 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://dtepdc:rivera!714@dtepdc-shard-00-00-xcmlo.mongodb.net:27017,dtepdc-shard-00-01-xcmlo.mongodb.net:27017,dtepdc-shard-00-02-xcmlo.mongodb.net:27017/dtepdc?ssl=true&replicaSet=dtepdc-shard-0&authSource=admin');
+
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var BearSchema   = new Schema({
+    name: String
+});
+
+module.exports = mongoose.model('Bear', BearSchema);
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
